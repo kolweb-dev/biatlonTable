8d5562462611d062e1fc83e0e6060edfb6845c2f
Mykola Tkachuk <mykola.tkachukki@gmail.com>
2021-04-02 21:57:41 +0300

    Updates

---
+++ precache-manifest.448322bf809a81551cbf02fddf1869ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b630add9c91c828e25f728a4dfd7b9a",
+    "url": "/kolweb-dev.io/biatlonTable/index.html"
+  },
+  {
+    "revision": "2d4451854db0c2db14fa",
+    "url": "/kolweb-dev.io/biatlonTable/static/css/main.3574ed02.chunk.css"
+  },
+  {
+    "revision": "0ba8fbbdc2991d1011b9",
+    "url": "/kolweb-dev.io/biatlonTable/static/js/2.2806cdfb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/kolweb-dev.io/biatlonTable/static/js/2.2806cdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4451854db0c2db14fa",
+    "url": "/kolweb-dev.io/biatlonTable/static/js/main.f41c6368.chunk.js"
+  },
+  {
+    "revision": "26f1eb3311c3f9314960",
+    "url": "/kolweb-dev.io/biatlonTable/static/js/runtime-main.154b4eb5.js"
+  }
+]);
